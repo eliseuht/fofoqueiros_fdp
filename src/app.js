5a7a3673649b3a07b5569c2b93279fa24c3090f8
vendaseliseu <vendaseliseu@gmail.com>
2016-12-10 00:48:16 -0200

    exemplos obtendo resultados da tela

--- src/app.js
+++ src/app.js
@@ -1,4 +1,33 @@
 import $ from 'jquery';
 import Rx from 'rxjs/Rx';
 
-console.log('Fofoqueiro Rodando...');
+//*********************************************************//
+//***** Exemplo 1 ****************************************//
+const $btn = $('#btn'); // só para economizar algumas linhas
+//Fofoqueiro escutando o Evento 'click'
+const btnStream$ = Rx.Observable.fromEvent($btn,'click');
+/* fofoca sendo gerada e recebendo como parametros ( se a fofoca deu certo , se deu errado ,se fofoca terminou) */
+btnStream$.subscribe(
+  fofoca=>{console.info('A criatura clicou no botão');},
+  zicou=>{console.error(zicou);},
+  ()=>console.log('Fim')
+);
+
+//********************************************************//
+//******Exemplo 2 ****************************************//
+const $output = $('#output');
+const $input = $('#input');
+const inputStream$ = Rx.Observable.fromEvent($input,'keyup');
+inputStream$.subscribe(
+  evento => {
+    console.log(evento.currentTarget.value);
+    $output.html(evento.currentTarget.value);
+  }
+);
+//*********************************************************//
+//******Exemplo 3 *****************************************//
+const $mouse = $('#mouse');
+const moveStream$ = Rx.Observable.fromEvent(document, 'mousemove');
+moveStream$.subscribe(
+  evento => $mouse.html('<b>X:</b> '+evento.clientX+'<b>Y:</b>'+evento.clientY)
+);
