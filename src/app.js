b15b59332ed06866f678e3201c6208a69e34a5e1
vendaseliseu <vendaseliseu@gmail.com>
2016-12-10 22:35:31 -0200

    Observable do zero

--- src/app.js
+++ src/app.js
@@ -1,40 +1,26 @@
 import $ from 'jquery';
 import Rx from 'rxjs/Rx';
-const numbers = [33, 44, 55, 66, 77];
-const numbers$ = Rx.Observable.from(numbers);
-numbers$.subscribe(
-    v => {
-        console.info(v);
-    },
-    err => {
-        console.log(err);
-    },
-    complete => {
-        console.log('Completed');
-    }
-);
-const posts = [{
-        title: 'Post One',
-        body: 'This is the body of Post One'
-    }, {
-        title: 'Post Two',
-        body: 'This is the body of Post Two'
-    }, {
-        title: 'Post Three',
-        body: 'This is the body of Post Three'
-    }
 
-];
-const posts$ = Rx.Observable.from(posts).subscribe(v => {
-    console.log(v);
-    $('#posts').append(`<li><h3>${v.title}</h3><p>${v.body}</p>`);
+const source$ = new Rx.Observable(observer => {
+    console.log('Creating Observable');
+    observer.next('Hello World');
+    observer.next('Another Value');
+    observer.error(new Error('Error: Something went wrong'));
+    setTimeout(() => {
+        observer.next('Yet another value');
+        observer.complete();
+    }, 3000);
 });
-
-const set = new Set(['Hello', 44, {
-    title: 'My title'
-}]);
-const set$ = Rx.Observable.from(set).subscribe(
-    v => {
-        console.log(v);
-    }
-);
+source$
+    .catch(err => Rx.Observable.of(err))
+    .subscribe(
+        x => {
+            console.log(x);
+        },
+        err => {
+            console.error(err);
+        },
+        () => {
+            console.log('Completed')
+        }
+    );
